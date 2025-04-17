ceff91e683e491374048c4526ac41f9d34b6963e
Mohamed Elbanna <moelbanna880@gmail.com>
2025-04-18 00:48:40 +0200

    feat(dashboard): implement DoctorDashboard with sidebar, stats cards, and appointments table

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
+import DoctorDashboard from './features/doctors/pages/doctor-dashboard'
 
 // Add icons to the library for global usage
 library.add(faUser, faHome)
@@ -11,8 +12,9 @@ function App() {
 
   return (
     <>
-      <p class="font-sans text-8xl text-blue-600/100 ">welcome to roshitta web app</p>
-      <FontAwesomeIcon icon="user" className="text-blue-500 text-4xl" />
+      {/* <p class="font-sans text-8xl text-blue-600/100 ">welcome to roshitta web app</p>
+      <FontAwesomeIcon icon="user" className="text-blue-500 text-4xl" /> */}
+      <DoctorDashboard/>
     </>
   )
 }
