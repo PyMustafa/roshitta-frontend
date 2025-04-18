a7b5de9956697129a5ff3b00f712ba7fe3a19051
Ra4ida <rasheadshokr@gmail.com>
2025-04-18 15:57:55 +0200

    "fix(footer): update footer layout"

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,19 +4,18 @@ const Footer = () => {
   return (
     <footer className="bg-white text-gray-700 pb-4 pt-8 px-6">
       {/* Main container with responsive layout for footer content */}
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-start items-start text-center md:text-left gap-8">
-        
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-start md:items-start text-center md:text-left gap-8">
+
         {/* First column - Logo and description */}
-        <div className="md:w-1/3 md:mr-auto md:ml-4 space-y-4 order-1">
+        <div className="w-full md:w-1/3 md:mr-auto md:ml-4 space-y-4 order-1 md:order-1">
           <h2 className="text-2xl font-bold text-[#4a5ae8]">Roshitta</h2>
           <p className="text-gray-600">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
           </p>
         </div>
 
         {/* Second column - Company navigation links */}
-        <div className="md:w-1/6 md:mr-4 md:ml-auto space-y-4 order-3">
+        <div className="w-full md:w-1/6 md:mr-4 md:ml-auto space-y-4 order-3 md:order-3">
           <h3 className="text-lg font-bold ">COMPANY</h3>
           <ul className="space-y-2">
             <li><a href="#" className="hover:text-[#09e5ab] transition">Home</a></li>
@@ -27,7 +26,7 @@ const Footer = () => {
         </div>
 
         {/* Third column - Contact information */}
-        <div className="md:w-1/6 md:ml-auto space-y-4 order-4">
+        <div className="w-full md:w-1/6 md:ml-auto space-y-4 order-4 md:order-4">
           <h3 className="text-lg font-bold ">GET IN TOUCH</h3>
           <ul className="space-y-2">
             <li className="flex justify-center md:justify-start items-center">
