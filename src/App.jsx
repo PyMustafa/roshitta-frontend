8ccc07c4e9b8de654eaaab6e33b6d9247025a12f
Mohamed Elbanna <moelbanna880@gmail.com>
2025-04-21 11:33:23 +0200

    feat: implement appointment scheduling feature with clinic selection and weekly availability

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
-import DoctorDashboard from './features/doctors/pages/doctor-dashboard'
+// import DoctorDashboard from './features/doctors/pages/doctor-dashboard'
+import { AppointmentPage } from './features/doctors/pages/AppointmentPage'
 
 // Add icons to the library for global usage
 library.add(faUser, faHome)
@@ -14,7 +15,7 @@ function App() {
     <>
       <p class="font-sans text-8xl text-blue-600/100 ">welcome to roshitta web app</p>
       <FontAwesomeIcon icon="user" className="text-blue-500 text-4xl" />
-      <DoctorDashboard/>
+      <AppointmentPage/>
     </>
   )
 }
