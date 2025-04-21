20a18db12f2482371ef65a3458b37f182ef8a385
Mohamed Elbanna <moelbanna880@gmail.com>
2025-04-21 11:34:56 +0200

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
-// import DoctorDashboard from './features/doctors/pages/doctor-dashboard'
-import { AppointmentPage } from './features/doctors/pages/AppointmentPage'
+import DoctorDashboard from './features/doctors/pages/doctor-dashboard'
 
 // Add icons to the library for global usage
 library.add(faUser, faHome)
@@ -15,7 +14,7 @@ function App() {
     <>
       <p class="font-sans text-8xl text-blue-600/100 ">welcome to roshitta web app</p>
       <FontAwesomeIcon icon="user" className="text-blue-500 text-4xl" />
-      <AppointmentPage/>
+      <DoctorDashboard />
     </>
   )
 }
